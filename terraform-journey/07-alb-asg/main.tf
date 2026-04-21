26d9d092a902a6f496b30beaf45b7fc6da69dce1
shashi <zero182004@gmail.com>
2026-04-21 17:31:35 +0530

    Feat: Automated CloudWatch observability using user_data (updated code)

--- terraform-journey/07-alb-asg/main.tf
+++ terraform-journey/07-alb-asg/main.tf
@@ -72,7 +72,7 @@ wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-
 dpkg -i amazon-cloudwatch-agent.deb || apt-get install -f -y
 mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
 
-cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+cat <<CONFIG > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 {
   "logs": {
     "logs_collected": {
@@ -88,11 +88,10 @@ cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
     }
   }
 }
-EOF
-
+CONFIG
 /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s
-  )
-
+EOF
+)
   tag_specifications {
     resource_type = "instance"
 
